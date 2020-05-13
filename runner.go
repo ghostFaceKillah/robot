e385a3b45c721215579d7cd733e8b27a1c427b4d
ghostFaceKillah <garmulewicz@gmail.com>
2020-05-13 20:10:44 +0200

    refactor runner

--- runner.go
+++ runner.go
@@ -2,23 +2,7 @@ package main
 
 import (
 	"fmt"
-	"robot/algo/raytrace"
 )
-
-
-func BresenhamSmokeTest(){
-	for _, x_side := range []int{-1, 1} {
-		for _, y_side := range []int{-1, 1} {
-			resu := raytrace.Bresenham2D(0, x_side * 7, 0, y_side * 4)
-
-			for _, thingy := range resu {
-				fmt.Println(thingy)
-			}
-
-		}
-	}
-}
-
 
 func BresenhamTranslationInvariance() {
 	// here we will see if we transpose by some integer ammount
@@ -26,9 +10,6 @@ func BresenhamTranslationInvariance() {
 
 func main() {
 	// ok kinda works
-
-
-
 
 }
 
