a6c3e42a2e512be99d342ec55b52b89e6d2c950b
ghostFaceKillah <garmulewicz@gmail.com>
2020-05-17 20:45:08 +0200

    protobuf next thing

--- py/sensor_pb2.py
+++ py/sensor_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='msg',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\x0csensor.proto\x12\x03msg\"F\n\x0cOdom2DUpdate\x12\x11\n\ttimestamp\x18\x01 \x01(\x03\x12\t\n\x01x\x18\x02 \x01(\x01\x12\t\n\x01y\x18\x03 \x01(\x01\x12\r\n\x05theta\x18\x04 \x01(\x01\"~\n\x0bLidar2DScan\x12\x11\n\ttimestamp\x18\x01 \x01(\x03\x12\x0c\n\x04scan\x18\x02 \x03(\x01\x12\x0f\n\x07n_scans\x18\x03 \x01(\r\x12\x11\n\tmin_range\x18\x04 \x01(\x01\x12\x11\n\tmax_range\x18\x05 \x01(\x01\x12\x17\n\x0fscan_resolution\x18\x06 \x01(\x01\"T\n\x0bNikolaiScan\x12\x1f\n\x04odom\x18\x01 \x01(\x0b\x32\x11.msg.Odom2DUpdate\x12$\n\nlidar_scan\x18\x02 \x01(\x0b\x32\x10.msg.Lidar2DScan\"3\n\x10NikolaiManyScans\x12\x1f\n\x05scans\x18\x01 \x03(\x0b\x32\x10.msg.NikolaiScanb\x06proto3')
+  serialized_pb=_b('\n\x0csensor.proto\x12\x03msg\"F\n\x0cOdom2DUpdate\x12\x11\n\ttimestamp\x18\x01 \x01(\x01\x12\t\n\x01x\x18\x02 \x01(\x01\x12\t\n\x01y\x18\x03 \x01(\x01\x12\r\n\x05theta\x18\x04 \x01(\x01\"~\n\x0bLidar2DScan\x12\x11\n\ttimestamp\x18\x01 \x01(\x01\x12\x0c\n\x04scan\x18\x02 \x03(\x01\x12\x0f\n\x07n_scans\x18\x03 \x01(\r\x12\x11\n\tmin_range\x18\x04 \x01(\x01\x12\x11\n\tmax_range\x18\x05 \x01(\x01\x12\x17\n\x0fscan_resolution\x18\x06 \x01(\x01\"O\n\x0bNikolaiScan\x12\x1f\n\x04odom\x18\x01 \x01(\x0b\x32\x11.msg.Odom2DUpdate\x12\x1f\n\x05lidar\x18\x02 \x01(\x0b\x32\x10.msg.Lidar2DScan\"3\n\x10NikolaiManyScans\x12\x1f\n\x05scans\x18\x01 \x03(\x0b\x32\x10.msg.NikolaiScanb\x06proto3')
 )
 
 
@@ -35,8 +35,8 @@ _ODOM2DUPDATE = _descriptor.Descriptor(
   fields=[
     _descriptor.FieldDescriptor(
       name='timestamp', full_name='msg.Odom2DUpdate.timestamp', index=0,
-      number=1, type=3, cpp_type=2, label=1,
-      has_default_value=False, default_value=0,
+      number=1, type=1, cpp_type=5, label=1,
+      has_default_value=False, default_value=float(0),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
@@ -87,8 +87,8 @@ _LIDAR2DSCAN = _descriptor.Descriptor(
   fields=[
     _descriptor.FieldDescriptor(
       name='timestamp', full_name='msg.Lidar2DScan.timestamp', index=0,
-      number=1, type=3, cpp_type=2, label=1,
-      has_default_value=False, default_value=0,
+      number=1, type=1, cpp_type=5, label=1,
+      has_default_value=False, default_value=float(0),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
@@ -159,7 +159,7 @@ _NIKOLAISCAN = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='lidar_scan', full_name='msg.NikolaiScan.lidar_scan', index=1,
+      name='lidar', full_name='msg.NikolaiScan.lidar', index=1,
       number=2, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
@@ -178,7 +178,7 @@ _NIKOLAISCAN = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=221,
-  serialized_end=305,
+  serialized_end=300,
 )
 
 
@@ -208,12 +208,12 @@ _NIKOLAIMANYSCANS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=307,
-  serialized_end=358,
+  serialized_start=302,
+  serialized_end=353,
 )
 
 _NIKOLAISCAN.fields_by_name['odom'].message_type = _ODOM2DUPDATE
-_NIKOLAISCAN.fields_by_name['lidar_scan'].message_type = _LIDAR2DSCAN
+_NIKOLAISCAN.fields_by_name['lidar'].message_type = _LIDAR2DSCAN
 _NIKOLAIMANYSCANS.fields_by_name['scans'].message_type = _NIKOLAISCAN
 DESCRIPTOR.message_types_by_name['Odom2DUpdate'] = _ODOM2DUPDATE
 DESCRIPTOR.message_types_by_name['Lidar2DScan'] = _LIDAR2DSCAN
